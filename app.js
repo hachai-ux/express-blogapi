a003771adfb56f616ed02d6240522596b1c2dc88
Hac Hai Pham <hpham.ux@gmail.com>
2022-02-17 13:17:14 +0100

    Add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -24,6 +25,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
